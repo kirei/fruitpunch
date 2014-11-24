a9765763dc7de18c20d230dcde73889244324028
Jakob Schlyter <jakob@kirei.se>
2014-11-24 12:08:28 +0100

    init

---
+++ yosemite.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+#
+# Sanitizer for OS X 10.10 (Yosemite)
+
+# do not save new documents to iCloud by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# disable gamed
+defaults write com.apple.gamed Disabled -bool true
