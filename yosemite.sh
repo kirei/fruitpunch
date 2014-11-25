d4acfccdb733a74869d936afbceb0d4ad75df390
Jakob Schlyter <jakob@kirei.se>
2014-11-25 09:26:52 +0100

    diable NetBIOS and Microsoft RPC

--- yosemite.sh
+++ yosemite.sh
@@ -2,6 +2,15 @@
 #
 # Sanitizer for OS X 10.10 (Yosemite)
 
+stop_and_unload() {
+	sudo launchctl stop -w $1
+	sudo launchctl unload -w $1
+}
+
+unload() {
+	sudo launchctl unload -w $1
+}
+
 # do not save new documents to iCloud by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
@@ -10,3 +19,14 @@ defaults write com.apple.gamed Disabled -bool true
 
 # no automatic captive portal detection
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -boolean false
+
+# NetBIOS
+stop_and_unload /System/Library/LaunchDaemons/com.apple.netbiosd.plist 
+
+# Microsoft RPC
+stop_and_unload /System/Library/LaunchDaemons/com.apple.msrpc.echosvc.plist
+stop_and_unload /System/Library/LaunchDaemons/com.apple.msrpc.lsarpc.plist
+stop_and_unload /System/Library/LaunchDaemons/com.apple.msrpc.mdssvc.plist
+stop_and_unload /System/Library/LaunchDaemons/com.apple.msrpc.netlogon.plist
+stop_and_unload /System/Library/LaunchDaemons/com.apple.msrpc.srvsvc.plist
+stop_and_unload /System/Library/LaunchDaemons/com.apple.msrpc.wkssvc.plist
